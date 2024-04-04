0b19e50e3408ef2e35e56158105e68920f471dae
Marijn Veenstra <114413125+MarijnVeenstra@users.noreply.github.com>
2024-04-04 09:52:09 +0200

    Fixed styles

--- app/src/Style.java
+++ app/src/Style.java
@@ -93,8 +93,18 @@ public class Style {
 	public void setLeading(int leading) {
 		this.leading = leading;
 	}
-
 	public static void createStyles() {
-		throw new UnsupportedOperationException("Unimplemented method 'createStyles'");
-	}
+		styles = new Style[5];    
+		// The styles are fixed.
+		styles[0] = new Style(0, Color.red,   48, 20);	// style for item-level 0
+		styles[1] = new Style(20, Color.blue,  40, 10);	// style for item-level 1
+		styles[2] = new Style(50, Color.black, 36, 10);	// style for item-level 2
+		styles[3] = new Style(70, Color.black, 30, 10);	// style for item-level 3
+		styles[4] = new Style(90, Color.black, 24, 10);	// style for item-level 4
+}
+
+
+	// public static void createStyles() {
+	// 	throw new UnsupportedOperationException("Unimplemented method 'createStyles'");
+	// }
 }
