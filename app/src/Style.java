b45830abbba381ecb75e68c9f34e006456011752
Marijn Veenstra <114413125+MarijnVeenstra@users.noreply.github.com>
2024-04-04 15:46:11 +0200

    Fixed decorator works yes test :)

--- app/src/Style.java
+++ app/src/Style.java
@@ -1,110 +1,80 @@
 import java.awt.Color;
 import java.awt.Font;
-
-/** <p>Style is for Indent, Color, Font and Leading.</p>
- * <p>Direct relation between style-number and item-level:
- * in Slide style if fetched for an item
- * with style-number as item-level.</p>
- * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
- * @version 1.1 2002/12/17 Gert Florijn
- * @version 1.2 2003/11/19 Sylvia Stuurman
- * @version 1.3 2004/08/17 Sylvia Stuurman
- * @version 1.4 2007/07/16 Sylvia Stuurman
- * @version 1.5 2010/03/03 Sylvia Stuurman
- * @version 1.6 2014/05/16 Sylvia Stuurman
- */
+import java.util.HashMap;
+import java.util.Map;
 
 public class Style {
-	private static Style[] styles; // de styles
-	
-	private static final String FONTNAME = "Helvetica";
-	int indent;
-	Color color;
-	Font font;
-	int fontSize;
-	int leading;
+    private static Map<Integer, Style> styles = new HashMap<>();
+    private static final int DEFAULT_STYLE_KEY = 0;
+    private static final String FONTNAME = "Helvetica";
 
-	public static Style getStyle(int level) {
-		if (level >= styles.length) {
-			level = styles.length - 1;
-		}
-		return styles[level];
-	}
+    private int indent;
+    private Color color;
+    private Font font;
+    private int fontSize;
+    private int leading;
 
-	public Style(int indent, Color color, int points, int leading) {
-		this.indent = indent;
-		this.color = color;
-		font = new Font(FONTNAME, Font.BOLD, fontSize=points);
-		this.leading = leading;
-	}
+    public Style(int indent, Color color, int size, int leading) {
+        this.indent = indent;
+        this.color = color;
+        font = new Font(FONTNAME, Font.BOLD, fontSize = size);
+        this.leading = leading;
+    }
 
-	public String toString() {
-		return "["+ indent + "," + color + "; " + fontSize + " on " + leading +"]";
-	}
+    public static void createStyle(int key, int indent, Color color, int size, int leading) {
+        styles.put(key, new Style(indent, color, size, leading));
+    }
 
-	public static Style[] getStyles() {
-		return styles;
-	}
+    public static Style getStyle(int key) {
+        return styles.getOrDefault(key, styles.get(DEFAULT_STYLE_KEY));
+    }
 
-	public static void setStyles(Style[] styles) {
-		Style.styles = styles;
-	}
+    public int getIndent() {
+        return indent;
+    }
 
-	public static String getFontname() {
-		return FONTNAME;
-	}
+    public void setIndent(int indent) {
+        this.indent = indent;
+    }
 
-	public int getIndent() {
-		return indent;
-	}
+    public Color getColor() {
+        return color;
+    }
 
-	public void setIndent(int indent) {
-		this.indent = indent;
-	}
+    public void setColor(Color color) {
+        this.color = color;
+    }
 
-	public Color getColor() {
-		return color;
-	}
+    public Font getFont(float scale) {
+        return font.deriveFont(fontSize * scale);
+    }
 
-	public void setColor(Color color) {
-		this.color = color;
-	}
+    public void setFont(Font font) {
+        this.font = font;
+    }
 
-	public Font getFont(float scale) {
-		return font.deriveFont(fontSize * scale);
-	}
+    public void setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+    }
 
-	public void setFont(Font font) {
-		this.font = font;
-	}
+    public int getLeading() {
+        return leading;
+    }
 
-	public int getFontSize() {
-		return fontSize;
-	}
+    public void setLeading(int leading) {
+        this.leading = leading;
+    }
 
-	public void setFontSize(int fontSize) {
-		this.fontSize = fontSize;
-	}
+    static {
+        createStyles();
+    }
 
-	public int getLeading() {
-		return leading;
-	}
-
-	public void setLeading(int leading) {
-		this.leading = leading;
-	}
-	public static void createStyles() {
-		styles = new Style[5];    
-		// The styles are fixed.
-		styles[0] = new Style(0, Color.red,   48, 20);	// style for item-level 0
-		styles[1] = new Style(20, Color.blue,  40, 10);	// style for item-level 1
-		styles[2] = new Style(50, Color.black, 36, 10);	// style for item-level 2
-		styles[3] = new Style(70, Color.black, 30, 10);	// style for item-level 3
-		styles[4] = new Style(90, Color.black, 24, 10);	// style for item-level 4
-}
-
-
-	// public static void createStyles() {
-	// 	throw new UnsupportedOperationException("Unimplemented method 'createStyles'");
-	// }
+    private static void createStyles() {
+        styles.put(DEFAULT_STYLE_KEY, new Style(0, Color.black, 12, 10));
+        styles.put(0, new Style(0, Color.red, 48, 20));   // style for item-level 0
+        styles.put(1, new Style(20, Color.blue, 40, 10)); // style for item-level 1
+        styles.put(2, new Style(50, Color.black, 36, 10)); // style for item-level 2
+        styles.put(3, new Style(70, Color.black, 30, 10)); // style for item-level 3
+        styles.put(4, new Style(90, Color.black, 24, 10)); // style for item-level 4
+    }
 }
